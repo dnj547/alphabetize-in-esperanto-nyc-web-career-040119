f474b2f3f3991b0b9f6615018a22be2fc55dff0f
Danielle Jasper <dnj547@live.com>
2019-03-18 23:49:24 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
-    ESPERANTO_ALPHABET.index(word)
+    ESPERANTO_ALPHABET.split.index(word)
   end
 end
