4ad3dcbea1c9e683a63b61046d408ec13e58ddcf
Danielle Jasper <dnj547@live.com>
2019-03-18 23:55:36 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,7 @@ require 'pry'
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
 def alphabetize(arr)
   arr.sort_by do |word|
+    binding.pry
     ESPERANTO_ALPHABET.index(word)
   end
 end
